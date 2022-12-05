7380a041148c74e45fd20b8745eb49546ed7a047
ASPIRINI1 <zverk3333@gmail.com>
2022-12-05 19:41:59 +0300

    Storyboard removed, SceneDelegate configured

--- GadgetShop/MainModule/Views/MainViewController.swift
+++ GadgetShop/MainModule/Views/MainViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 class MainViewController: UIViewController {
     
     private lazy var categoriesCollectionView = {
-        let view = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: CategoryCollectionViewCell.self))
         return view
     }()
     private lazy var hotSalesCollectionView = {
-        let view = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.register(HotSalesCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: HotSalesCollectionViewCell.self))
         return view
     }()
     private lazy var bestSellerCollectionView = {
-        let view = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.register(BestSellerProductCollectionViewCell.self, forCellWithReuseIdentifier: String(describing: BestSellerProductCollectionViewCell.self))
         return view
     }()
