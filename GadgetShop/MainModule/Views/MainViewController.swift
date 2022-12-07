ad5d4f4fdf652c4a25c1f8d85e05915bc0f8cd2f
ASPIRINI1 <zverk3333@gmail.com>
2022-12-07 21:12:07 +0300

    MainViewModel initial setup

--- GadgetShop/MainModule/Views/MainViewController.swift
+++ GadgetShop/MainModule/Views/MainViewController.swift
@@ -31,10 +31,21 @@ class MainViewController: UIViewController {
         let view = UIStackView()
         return view
     }()
-
+    private let viewModel: MainViewModelProtocol
+    
+    init(viewModel: MainViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        viewModel.viewLoaded()
     }
     
     private func setupView() {
---
+++ GadgetShop/MainModule/Views/MainViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewModel.swift
+//  GadgetShop
+//
+//  Created by Станислав Зверьков on 07.12.2022.
+//
+
+import Foundation
+
+protocol MainViewModelProtocol {
+    var updateData: (() -> ())? { get set }
+    var productList: ProductList? { get set }
+    init(networkService: NetwokrServiceProtocol, coordinator: MainFlowCoordinatorProtocol)
+    func viewLoaded()
+    func cartSelected()
+    func productSelected(productIndex: Int)
+}
+
+final class MainViewModel: MainViewModelProtocol {
+    
+    var updateData: (() -> ())?
+    var productList: ProductList?
+    var networkService: NetwokrServiceProtocol
+    unowned var coordinator: MainFlowCoordinatorProtocol
+    
+    init(networkService: NetwokrServiceProtocol, coordinator: MainFlowCoordinatorProtocol) {
+        self.networkService = networkService
+        self.coordinator = coordinator
+    }
+    
+    func viewLoaded() {
+        
+    }
+    
+    func cartSelected() {
+        coordinator.presentCart()
+    }
+    
+    func productSelected(productIndex: Int) {
+        
+    }
+}
