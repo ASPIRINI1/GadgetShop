355e5eccf51d16882f3800cd4b6dba03dd8fd71c
ASPIRINI1 <zverk3333@gmail.com>
2022-12-11 00:20:12 +0300

    UIColor+Hex extension refactoring

--- GadgetShop/Extensions/UIColor+Hex.swift
+++ GadgetShop/Extensions/UIColor+Hex.swift
@@ -8,21 +8,31 @@
 import UIKit
 
 extension UIColor {
-    func hexStringToUIColor (hex:String) -> UIColor {
+    convenience init?(_ hex:String) {
         var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        if (cString.hasPrefix("#")) {
+        if cString.hasPrefix("#") {
             cString.remove(at: cString.startIndex)
         }
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
+        guard cString.count == 6 else { return nil }
         var rgbValue:UInt64 = 0
         Scanner(string: cString).scanHexInt64(&rgbValue)
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
+        self.init(displayP3Red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: 1.0)
+    }
+    
+    enum CustomColor: String {
+        case orange = "FF6E4E"
+        case pink = "010035"
+        
+        var uiColor: UIColor? {            
+            switch self {
+            case .orange:
+                return .init(CustomColor.orange.rawValue)
+            case .pink:
+                return .init(CustomColor.pink.rawValue)
+            }
+        }
     }
 }
