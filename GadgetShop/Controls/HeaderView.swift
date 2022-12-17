ab22736a955e5ec07c8ff8a55825344d8ea40b5c
ASPIRINI1 <zverk3333@gmail.com>
2022-12-17 17:39:12 +0300

    HeaderView control setuped

---
+++ GadgetShop/Controls/HeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  HeaderView.swift
+//  GadgetShop
+//
+//  Created by Станислав Зверьков on 15.12.2022.
+//
+
+import UIKit
+
+class HeaderView: UIStackView {
+
+    private var leftButton = UIButton()
+    private var rightButton = UIButton()
+    private var titleLabel = UILabel()
+    
+    init(leftButton: UIButton?, title: String?, rightButton: UIButton?) {
+        super.init(frame: .zero)
+        axis = .horizontal
+        distribution = .equalCentering
+        alignment = .center
+        if let leftButton = leftButton {
+            self.leftButton = leftButton
+        }
+        if let rightButton = rightButton {
+            self.rightButton = rightButton
+        }
+        self.titleLabel.text = title
+        addArrangedSubview(self.leftButton)
+        addArrangedSubview(titleLabel)
+        addArrangedSubview(self.rightButton)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
