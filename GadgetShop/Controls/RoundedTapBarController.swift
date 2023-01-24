4a8e4dc2ae19f1ebd3d7a60b8693083aca3b7038
ASPIRINI1 <zverk3333@gmail.com>
2023-01-24 14:43:37 +0300

    RoundedTapBarController codeStyle fix

--- GadgetShop/Controls/RoundedTapBarController.swift
+++ GadgetShop/Controls/RoundedTapBarController.swift
@@ -18,7 +18,7 @@ class RoundedTapBarController: UITabBarController {
         tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         tabBar.layer.masksToBounds = true
         tabBar.unselectedItemTintColor = .white
-        }
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
